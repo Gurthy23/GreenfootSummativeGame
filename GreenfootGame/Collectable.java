b7310d20fa4747d5c1170e833174802049baebe6
DwaynededwedwibbleXS <106257504+DwaynededwedwibbleXS@users.noreply.github.com>
2022-06-13 10:58:31 -0400

    Working on collectable animation

--- GreenfootGame/Collectable.java
+++ GreenfootGame/Collectable.java
@@ -1,3 +1,4 @@
+
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
@@ -8,12 +9,34 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Collectable extends Actor
 {
+    GreenfootImage image1 = new GreenfootImage("Coin/sprite_collectable0.png");
+    GreenfootImage image2 = new GreenfootImage("Coin/sprite_collectable1.png");
+    GreenfootImage image3 = new GreenfootImage("Coin/sprite_collectable2.png");
+    GreenfootImage image4 = new GreenfootImage("Coin/sprite_collectable3.png");
+    
+    int frameCounter = 0;
     /**
      * Act - do whatever the Collectable wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
+        frameCounter++;
+        if (frameCounter == 1)
+        {
+            setImage(image1);
+        }
+        if (frameCounter == 2)
+        {
+            setImage(image1);
+        }
+        if (frameCounter == 3)
+        {
+            setImage(image1);
+        }
+        if (frameCounter == 4)
+        {
+            setImage(image1);
+        }
     }    
 }
